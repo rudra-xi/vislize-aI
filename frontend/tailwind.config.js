a61e15deaf32e08a17c5daeceeb46d13ceb5b714
rudra-xi <goutam.prasad.2707@gmail.com>
2024-12-31 21:26:03 +0530

    custom color & shadow added

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -2,7 +2,18 @@
 export default {
 	content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
 	theme: {
-		extend: {},
+		extend: {
+			colors: {
+				primary: "#001b32",
+				secondary: "#092b50",
+				tertiary: "#579c8a",
+				quaternary: "#dbfcd1",
+				white: "#fbfbef",
+			},
+			dropShadow:{
+				glow: "3px 3px 5px rgba(219, 252, 209, 0.2)"
+			}
+		},
 	},
 	plugins: [],
 };
