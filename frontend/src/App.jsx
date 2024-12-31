cb1f7b2f3bab08728c7c0ae8212403e6fe4f6ed1
rudra-xi <goutam.prasad.2707@gmail.com>
2025-01-01 00:29:45 +0530

    website layout and main style done

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,13 +1,17 @@
 import React from "react";
 import { Route, Routes } from "react-router-dom";
 
+import Navbar from "./Components/Navbar.jsx";
 import Home from "./Pages/Home";
 import Result from "./Pages/Result";
 import BuyCredit from "./Pages/BuyCredit";
 
+import { styles } from "./Assets/styles";
+
 function App() {
 	return (
-		<section>
+		<section className="px-4 sm:px-10 md:px-14 lg:px-28 min-h-screen bg-gradient-to-t from-primary to-secondary text-white">
+			<Navbar />
 			<Routes>
 				<Route exact path="/" element={<Home />} />
 				<Route exact path="/result" element={<Result />} />
