b171d9c826e0b82a7ed2f2248cc598e720c17664
rudra-xi <goutam.prasad.2707@gmail.com>
2025-01-02 13:35:06 +0530

    logo size change

--- frontend/src/Components/Navbar.jsx
+++ frontend/src/Components/Navbar.jsx
@@ -5,24 +5,29 @@ import { AppContext } from "../Context/AppContext";
 
 import { assets } from "../Assets/assets";
 
+// Navbar component for navigation and user interaction
 function Navbar() {
 	const navigate = useNavigate();
 
-	const {user} = useContext(AppContext)
+	// Access user data from the AppContext
+	const { user } = useContext(AppContext);
 
 	return (
-		<nav className="flex items-center justify-between w-full py-5">
+		<nav className="flex items-center justify-between w-full py-4">
+			{/* Logo linking to the homepage */}
 			<Link to="/">
 				<img
 					src={assets.logo}
 					alt="logo"
-					className="w-32 sm:w-[150px] lg:w-40"
+					className="w-30 sm:w-[125px] lg:w-38"
 				/>
 			</Link>
 
 			<div>
+				{/* Conditional rendering based on user authentication status */}
 				{user ? (
 					<div className="flex items-center gap-2 sm:gap-3">
+						{/* Button to navigate to the buy credit page */}
 						<button
 							className="flex items-center gap-2 px-4 sm:px-5 py-1 sm:py-3 rounded-full bg-quaternary hover:scale-105 transition-all duration-500"
 							onClick={() => navigate("/buy-credit")}
@@ -36,9 +41,11 @@ function Navbar() {
 								Credit Left: 10
 							</p>
 						</button>
+						{/* Greeting message for the user */}
 						<p className="max-sm:hidden pl-4 text-white">
 							Hi, User
 						</p>
+						{/* User profile icon with a dropdown menu */}
 						<div className="relative group ">
 							<img
 								src={assets.profile_icon}
@@ -56,12 +63,14 @@ function Navbar() {
 					</div>
 				) : (
 					<div className="flex items-center gap-2 sm:gap-5">
+						{/* Link to the pricing page */}
 						<p
 							className="cursor-pointer "
 							onClick={() => navigate("/buy-credit")}
 						>
 							Pricing
 						</p>
+						{/* Login button for unauthenticated users */}
 						<button className="bg-tertiary px-7 py-2 sm:px-10 text-sm rounded-full">
 							Login
 						</button>
