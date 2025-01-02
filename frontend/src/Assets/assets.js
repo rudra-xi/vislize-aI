a5d6c4ab752e0ad5cd0451b41d47ccdfd50445ef
rudra-xi <goutam.prasad.2707@gmail.com>
2025-01-02 13:09:15 +0530

    new images added

--- frontend/src/Assets/assets.js
+++ frontend/src/Assets/assets.js
@@ -3,10 +3,13 @@ import logo_icon from "./logo_icon.svg";
 import facebook_icon from "./facebook-icon.svg";
 import instagram_icon from "./instagram-icon.svg";
 import twitter_icon from "./twitter-icon.svg";
-import star_icon from "./star_icon.svg";
+import star_icon from "./star-icon.svg";
 import rating_star from "./rating_star.svg";
-import sample_img_1 from "./sample_img_1.png";
-import sample_img_2 from "./sample_img_2.png";
+import monster_1 from "./monster-1.jpeg";
+import monster_2 from "./monster-2.jpeg";
+import monster_3 from "./monster-3.jpeg";
+import monster_4 from "./monster-4.jpeg";
+import monster_5 from "./monster-5.jpeg";
 import profile_img_1 from "./profile_img_1.png";
 import profile_img_2 from "./profile_img_2.png";
 import step_icon_1 from "./step_icon_1.svg";
@@ -15,7 +18,7 @@ import step_icon_3 from "./step_icon_3.svg";
 import email_icon from "./email_icon.svg";
 import lock_icon from "./lock_icon.svg";
 import cross_icon from "./cross_icon.svg";
-import star_group from "./star_group.png";
+import star_group from "./star-group.svg";
 import credit_icon from "./credit-icon.svg";
 import profile_icon from "./profile-icon.svg";
 
@@ -25,10 +28,13 @@ export const assets = {
 	facebook_icon,
 	instagram_icon,
 	twitter_icon,
-	star_icon, 
+	star_icon,
 	rating_star,
-	sample_img_1,
-	sample_img_2,
+	monster_1,
+	monster_2,
+	monster_3,
+	monster_4,
+	monster_5,
 	email_icon,
 	lock_icon,
 	cross_icon,
@@ -36,6 +42,14 @@ export const assets = {
 	credit_icon,
 	profile_icon,
 };
+
+export const monstersData = [
+	{ image: monster_1, id: 1 },
+	{ image: monster_2, id: 2 },
+	{ image: monster_3, id: 3 },
+	{ image: monster_4, id: 4 },
+     { image: monster_5, id: 5 },
+];
 
 export const stepsData = [
 	{
