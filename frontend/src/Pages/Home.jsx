796eafbcae8df46fd696326b90f9d82caaabbb4e
rudra-xi <goutam.prasad.2707@gmail.com>
2025-01-03 17:54:21 +0530

    home page done

--- frontend/src/Pages/Home.jsx
+++ frontend/src/Pages/Home.jsx
@@ -1,10 +1,18 @@
 import React from "react";
 import Header from "../Components/Header";
+import Steps from "../Components/Steps";
+import Description from "../Components/Description";
+import Testimonials from "../Components/Testimonials";
+import GenerateBtn from "../Components/GenerateBtn";
 
 function Home() {
 	return (
 		<section>
 			<Header />
+			<Steps />
+			<Description />
+			<Testimonials />
+			<GenerateBtn />
 		</section>
 	);
 }
