5d8b06c901db8026685692096fee9dccfa25ce61
rudra-xi <goutam.prasad.2707@gmail.com>
2025-01-03 17:52:43 +0530

    gen btn added

---
+++ frontend/src/Components/GenerateBtn.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+import Button from "../Reuseable/Button";
+
+function GenerateBtn() {
+	return (
+		<section>
+			<div className="pb-16 text-center">
+				<h3 className="text-2xl md:text-3xl lg:text-4xl mt-4 font-semibold py-4">
+					Witness the Wonder - Try Today!
+				</h3>
+				<Button />
+			</div>
+		</section>
+	);
+}
+
+export default GenerateBtn;
